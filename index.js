f70808c4cf02f322c1de27575b345943f95e9630
Joshua Campos <jdeveloper1203@gmail.com>
2022-08-29 11:47:54 -0400

    commit tree fd6ac7a4671d6e64000c37b4f9eccbb78f34cd03

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(start){
+    const blocks = 42 - start;
+
+    return Math.abs(blocks);
+};
+
+function distanceFromHqInFeet(start){
+    const feet =  (42 - start)* 264;
+
+    return Math.abs(feet);
+};
+
+function distanceTravelledInFeet(start, destination){
+    const feet = (destination - start)*264;
+
+    return Math.abs(feet);
+};
+
+function calculatesFarePrice(start, destination){
+    const feet = distanceTravelledInFeet(start, destination);
+
+    if (feet >= 400 && feet <= 2000){
+        return (feet - 400) * 0.02;
+    } else if (feet > 2000 && feet < 2500){
+        return 25;
+    } else if (feet > 2500){
+        return "cannot travel that far";
+    } else {
+        return 0;
+    }
+};
+
